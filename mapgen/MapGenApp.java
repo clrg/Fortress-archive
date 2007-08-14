3483a1114cdd298e8061f093073e4c34e812938b
dizzutch <dizzutch@a1603185-e031-0410-9a4d-82be57dd2110>
2007-08-14 21:56:35 +0000

    Added Application, there are some normalization errors, but I will work on those later.

---
+++ mapgen/MapGenApp.java
@@ -0,0 +1,37 @@
+import java.awt.*;
+import java.awt.geom.*;
+import javax.swing.*;
+
+public class MapGenApp extends JPanel {
+    private MapGenerator map;
+
+    public void paintComponent(Graphics g) {
+        clear(g);
+        Graphics2D g2d = (Graphics2D)g;
+        for(int h = 0; h < map.getHeight(); h++) {
+            for(int w = 0; w < map.getWidth(); w++) {
+                double c = map.getMapSection(h,w).getHeight();
+                float nc = 1 - (float)c;
+                g2d.setColor(new Color(nc,nc,nc));
+                g2d.draw(new Ellipse2D.Double((double)h,(double)w,1.0,1.0));
+            }
+        }
+    }
+    
+    protected void clear(Graphics g) {
+        super.paintComponent(g);
+    }
+    
+    public static void main(String[] args) {
+        MapGenApp app = new MapGenApp();
+        app.map = new MapGenerator(600,600);
+        app.map.generateMap_HillAlgorithm(5);
+        JFrame frame = new JFrame("mapgen");
+        frame.setBackground(Color.white);
+        app.setBackground(Color.white);
+        frame.setSize(600,600);
+        frame.setContentPane(app);
+        frame.addWindowListener(new ExitListener());
+        frame.setVisible(true);
+    }
+}
--- mapgen/MapGenerator.java
+++ mapgen/MapGenerator.java
@@ -38,20 +38,47 @@ public class MapGenerator {
         }
     }
 
-    public void generateMap_HillAlgorithm() {
+    public void generateMap_HillAlgorithm(int iterations) {
         Random rand = new Random();
-        for (int i = 0; i < 1; i++) {
+        double min = 0;
+        double max = 0;
+        for (int i = 0; i < iterations; i++) {
             //generate random centerpoint
             int x1 = rand.nextInt(this.height);
             int y1 = rand.nextInt(this.width);
             //generate random radius
-            int radius = rand.nextInt(this.width/2);
+            int radius = rand.nextInt(this.width/2) + 1;
+            double radius2 = java.lang.Math.pow(radius, 2);
             //for each point on the map, calculate height
             for (int x2 = 0; x2 < this.height; x2++) {
                 for (int y2 = 0; y2 < this.width; y2++) {
-                    map[x2][y2].setHeight((float)java.lang.Math.pow(radius, 2) - ((float)java.lang.Math.pow(x2-x1,2)) + ((float)java.lang.Math.pow(y2-y1,2)));
+                    double left = java.lang.Math.pow((x2-x1),2);
+                    double right = java.lang.Math.pow((y2-y1),2);
+                    double value = radius2 - (left + right);
+                    if (value >= 0) {
+                        min = (value < min) ? value : min;
+                        max = (value > max) ? value : max;
+                        double h = map[x2][y2].getHeight();
+                        map[x2][y2].setHeight(value + h);
+                    }
                 }
             }
         }
+        //normalize
+        System.out.println("max : "+ max);
+        System.out.println("min : "+ min);
+        double nmin = 0;
+        double nmax = 0;
+        for (int x = 0; x < this.height; x++) {
+             for (int y = 0; y < this.width; y++) {
+                 double h = map[x][y].getHeight();
+                 double value = ((h - min) / (max - min));
+                 nmin = (value < nmin) ? value : nmin;
+                 nmax = (value > nmax) ? value : nmax;
+                 map[x][y].setHeight(value);
+             }
+        }
+        System.out.println("nmax : "+ nmax);
+        System.out.println("nmin : "+ nmin);
     }
 }
