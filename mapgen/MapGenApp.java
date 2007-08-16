04672729943e486998b3940f7e8b9667eac74995
dizzutch <dizzutch@a1603185-e031-0410-9a4d-82be57dd2110>
2007-08-16 14:54:16 +0000

    made some fixes

--- mapgen/MapGenApp.java
+++ mapgen/MapGenApp.java
@@ -12,7 +12,12 @@ public class MapGenApp extends JPanel {
             for(int w = 0; w < map.getWidth(); w++) {
                 double c = map.getMapSection(h,w).getHeight();
                 float nc = 1 - (float)c;
-                g2d.setColor(new Color(nc,nc,nc));
+                try {
+                    g2d.setColor(new Color(nc,nc,nc));
+                } catch (java.lang.IllegalArgumentException e) {
+                    System.out.println(h + " " + w + " " + nc);
+                    g2d.setColor(new Color(255,0,0));
+                }
                 g2d.draw(new Ellipse2D.Double((double)h,(double)w,1.0,1.0));
             }
         }
@@ -24,8 +29,9 @@ public class MapGenApp extends JPanel {
     
     public static void main(String[] args) {
         MapGenApp app = new MapGenApp();
+        int iterations = new Integer(args[0]);
         app.map = new MapGenerator(600,600);
-        app.map.generateMap_HillAlgorithm(1000);
+        app.map.generateMap_HillAlgorithm(iterations);
         JFrame frame = new JFrame("mapgen");
         frame.setBackground(Color.white);
         app.setBackground(Color.white);
